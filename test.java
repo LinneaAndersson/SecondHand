5fc188900c9f4cd9483e2d4deb6abee4b2931bba
Andreas <andreas.wahlstrom@hotmail.com>
2013-03-20 11:33:49 +0100

    error

--- test.java
+++ test.java
@@ -1,7 +1,14 @@
 
 public class test {
     public static void main(String[] a) {
-	System.out.println("Hello world!");
+	System.out.println("Hello world!");//test
+
+
+
+
+
+
+
 	int b = 4;
     }
 }

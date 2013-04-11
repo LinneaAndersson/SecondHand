3df17b1ec2c9d41e1be8c38512dbb885efa3f14d
Eric Arnebäck <erkastina@hotmail.com>
2013-04-11 08:57:44 +0200

    Now the script for converting the documentation to PDF works on Linux as well.

--- convert_all_odt_documents_to_pdf.sh
+++ convert_all_odt_documents_to_pdf.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
-/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to pdf --outdir rad/ rad/*.odt
-/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to pdf --outdir meeting/ meeting/*.odt
+platform='unknown'
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+    # if you're on linux I assume you have libreoffice on your path.
+    libre_office_path='libreoffice'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   # If on OS X I assume you have put LibreOffice.app in the Applications/ directory
+    libre_office_path='/Applications/LibreOffice.app/Contents/MacOS/soffice'
+fi
+
+declare -a arr=(rad meeting)
+
+for dir in ${arr[@]}
+do
+    $libre_office_path --headless --convert-to pdf --outdir $dir $dir/*.odt
+done
+
+#$libre_office_path --headless --convert-to pdf --outdir meeting/ meeting/*.odt

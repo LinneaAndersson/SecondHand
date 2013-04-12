574f778f272cdfaa73d0e657950a86612d9201fa
Andreas <andreas.wahlstrom@hotmail.com>
2013-04-12 11:19:58 +0200

    update Universe&Level
    Signed-off-by: Andreas <andreas.wahlstrom@hotmail.com>

--- Twist/src/com/secondhand/twirl/Level.java
+++ Twist/src/com/secondhand/twirl/Level.java
@@ -1,5 +1,12 @@
 package com.secondhand.twirl;
 
+import java.util.List;
+
 public class Level {
 
+	private List entities;
+	
+	public Level(){
+		
+	}
 }
--- Twist/src/com/secondhand/twirl/Universe.java
+++ Twist/src/com/secondhand/twirl/Universe.java
@@ -1,5 +1,11 @@
 package com.secondhand.twirl;
 
 public class Universe {
-
+	private Level currentLevel;
+	
+	public Universe(){
+		
+	}
+	
+	
 }

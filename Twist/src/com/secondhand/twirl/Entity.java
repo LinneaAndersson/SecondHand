e8cbacd2226c9f63e2d451fa0f73a339de04b3f6
LinneaAndersson <Linneamaria10@gmail.com>
2013-04-12 14:03:26 +0200

    Created set and get-methods

--- Twist/src/com/secondhand/twirl/Entity.java
+++ Twist/src/com/secondhand/twirl/Entity.java
@@ -1,5 +1,32 @@
 package com.secondhand.twirl;
 
 public abstract class Entity {
-
+	private Position pos;
+	private Vector vector;
+	private int radius;
+	
+	public void setRadius(int r){
+		radius=r;
+	}
+	
+	public void setPosition(int x,int y){
+		pos.setPosition(x, y);
+	}
+	
+	public void setVector(){
+		
+	}
+	
+	public int getRadius(){
+		return radius;
+	}
+	
+	public Position getPosition(){
+		return pos;
+	}
+	
+	public Vector getVector(){
+		return vector;
+	}
+	
 }

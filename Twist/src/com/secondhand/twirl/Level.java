79b53269ff74b00410e52badbd04cf0495e40e69
Andreas <andreas.wahlstrom@hotmail.com>
2013-04-12 12:38:37 +0200

    Mer metoder
    Signed-off-by: Andreas <andreas.wahlstrom@hotmail.com>

--- Twist/src/com/secondhand/twirl/Level.java
+++ Twist/src/com/secondhand/twirl/Level.java
@@ -2,12 +2,26 @@ package com.secondhand.twirl;
 
 import java.util.List;
 
+import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
+
 public class Level {
 
 	private List<Entity> entities;
 	private int maxSize;
+	private PhysicsWorld pW;
+	
+	public Level(){
+		this(100);
+	}
 	
 	public Level(int maxSize){
-		this.maxSize = maxSize ;
+		this(maxSize, new PhysicsWorld(null, false));
 	}
+	
+	public Level(int maxSize, PhysicsWorld pW){
+		this.maxSize = maxSize;
+		this.pW = pW;
+	}
+	
+	
 }
--- Twist/src/com/secondhand/twirl/Universe.java
+++ Twist/src/com/secondhand/twirl/Universe.java
@@ -1,11 +1,23 @@
 package com.secondhand.twirl;
 
 public class Universe {
-	private Level currentLevel;
+	private static Level currentLevel;
+	private static int levelNbr = 0;
 	
 	public Universe(){
-		
+		currentLevel = new Level();
 	}
 	
+	public int getlvlNbr(){
+		return levelNbr;
+	}
+	
+	public Level getLevel(){
+		return currentLevel;
+	}
+	
+	public void NextLevel(){
+		currentLevel = new Level();
+	}
 	
 }

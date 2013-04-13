892a482bec4c6050dc66ac48c228dd093fa89cee
Johan Gerdin <johan@localhost.localdomain>
2013-04-13 11:37:47 +0200

    Added constructor

--- Twist/src/com/secondhand/twirl/Planet.java
+++ Twist/src/com/secondhand/twirl/Planet.java
@@ -1,7 +1,13 @@
 package com.secondhand.twirl;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class Planet extends Entity {
 
+	public Planet (Vector2 vector, int radius) {
+		super(vector,radius);
+	}
+	
 	@Override
 	public Entity_enum getEnum() {
 		// TODO Auto-generated method stub

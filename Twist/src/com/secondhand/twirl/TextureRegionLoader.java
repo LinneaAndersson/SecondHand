90af7f5920f9c12babbc27842148ec297a5f2fa8
Eric Arnebäck <erkastina@hotmail.com>
2013-04-13 08:54:35 +0200

    Added a TextureRegionLoader class for greatly simplifying the loading of TextureRegions.

---
+++ Twist/src/com/secondhand/twirl/TextureRegionLoader.java
@@ -0,0 +1,48 @@
+package com.secondhand.twirl;
+
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+
+
+/**
+ * Singelton class that makes loading TextureRegions much more convenient. 
+ */
+public class TextureRegionLoader extends Loader {
+	
+	private static TextureRegionLoader instance = null;
+	
+	public static TextureRegionLoader getInstance() {
+		if(instance == null) {
+			instance = new TextureRegionLoader();
+		}
+		return instance;
+	}
+	
+	public void setAssetBasePath(final String path) {
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath(path);
+	}
+	
+	public TextureRegion loadTextureRegion(final String fileName, final int width, 
+			final int height, final TextureOptions textureOptions) {
+		
+		BitmapTextureAtlas texture = new BitmapTextureAtlas(width, height, textureOptions);
+    	TextureRegion textureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(
+    			texture, this.context, fileName, 0, 0);
+    	
+    	this.engine.getTextureManager().loadTexture(texture);
+    	
+    	return textureRegion;
+	}
+
+	public TextureRegion loadTextureRegion(final String fileName, final int width, final int height) {
+		return loadTextureRegion(fileName, width, height, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+	}
+	
+
+	@Override
+	public String getDefaultAssetBasePath() {
+		return "gfx/";
+	}
+}

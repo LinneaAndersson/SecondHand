4a269eceeaa71a084fd1edbe10e52ba0c04c56ed
Johan Gerdin <johan@localhost.localdomain>
2013-04-13 12:03:26 +0200

    Added constructor to GameScene

--- Twist/src/com/secondhand/twirl/GameScene.java
+++ Twist/src/com/secondhand/twirl/GameScene.java
@@ -1,7 +1,17 @@
 package com.secondhand.twirl;
 
+import java.util.List;
+
+import org.anddev.andengine.entity.IEntity;
 import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
 
 public class GameScene extends Scene {
+
+	public GameScene (List<IEntity> entities, ColorBackground background) {
+		for (IEntity entity : entities)
+			attachChild(entity);
+		setBackground(background);
+	}
 	
 }

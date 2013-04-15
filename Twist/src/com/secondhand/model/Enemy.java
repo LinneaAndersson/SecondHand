6c18c79434f88f14d8ed4aac52713a3c6b0b5947
Eric Arnebäck <erkastina@hotmail.com>
2013-04-15 15:20:13 +0200

    float used instead of int for radius.

--- Twist/src/com/secondhand/model/Enemy.java
+++ Twist/src/com/secondhand/model/Enemy.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.math.Vector2;
 public class Enemy extends BlackHole {
 	private boolean isBigger;
 	
-	public Enemy (Vector2 vector, int radius) {
+	public Enemy (Vector2 vector, float radius) {
 		super(vector,radius);
 	}
 	

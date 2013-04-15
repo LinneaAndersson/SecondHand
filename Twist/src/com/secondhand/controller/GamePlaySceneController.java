629dc43dc3de50f2d0c5000affd7e6cd189950ec
Johan Gerdin <johan@localhost.localdomain>
2013-04-15 15:36:54 +0200

    added some comments and a player sprite

--- Twist/src/com/secondhand/controller/GamePlaySceneController.java
+++ Twist/src/com/secondhand/controller/GamePlaySceneController.java
@@ -5,17 +5,24 @@ import org.anddev.andengine.entity.scene.Scene.IOnSceneTouchListener;
 import org.anddev.andengine.input.touch.TouchEvent;
 
 import com.secondhand.model.Universe;
+import com.secondhand.opengl.Circle;
 import com.secondhand.scene.GamePlayScene;
 
 public class GamePlaySceneController {
 
+	// View
 	private GamePlayScene scene;
 	
+	// Model
 	private Universe universe;
+	
+	// Player sprite
+	private Circle player;
 	
 	public GamePlaySceneController(Scene gamePlayScene) {
 		scene = (GamePlayScene) gamePlayScene;
 		universe = Universe.getInstance();
+		player = new Circle(0, 0, universe.getLevel().getPlayer().getRadius());
 	}
 	
 	private class GameSceneTouchListener implements IOnSceneTouchListener{

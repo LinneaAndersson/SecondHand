265842fe565b6fdb1e5c84afddb23470e74de62d
Andreas <andreas.wahlstrom@hotmail.com>
2013-04-16 12:45:12 +0200

    Added skeleton testClasses for universe and Level

---
+++ TwirlTestTest/src/com/secondhand/model/LevelTest.java
@@ -0,0 +1,26 @@
+package com.secondhand.model;
+
+import junit.framework.TestCase;
+
+public class LevelTest extends TestCase {
+
+	public void testConstructor(){
+		
+	}
+	
+	public void testRegisterEntities(){
+		
+	}
+	
+	public void testRegisterEntity(){
+		
+	}
+	
+	public void testMoveEntities(){
+		
+	}
+	
+	public void TestcheckPlayerBigEnough(){
+		
+	}
+}
---
+++ TwirlTestTest/src/com/secondhand/model/UniverseTest.java
@@ -0,0 +1,14 @@
+package com.secondhand.model;
+
+import junit.framework.TestCase;
+
+public class UniverseTest extends TestCase {
+	
+	public void testUpdate(){
+		
+	}
+	
+	public void testNextLevel(){
+		
+	}
+}

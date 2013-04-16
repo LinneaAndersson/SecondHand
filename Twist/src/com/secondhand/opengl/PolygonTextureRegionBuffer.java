6ec822ac889b8f2a65cb20b9d18d16e6672c09b3
Eric Arnebäck <erkastina@hotmail.com>
2013-04-16 17:20:05 +0200

    Added a TexturedCircle class for textured circles. Added PolygonTextureRegionBuffer since it is used by TexturedCircle.

---
+++ Twist/src/com/secondhand/opengl/PolygonTextureRegionBuffer.java
@@ -0,0 +1,60 @@
+package com.secondhand.opengl;
+
+import java.util.List;
+
+import org.anddev.andengine.opengl.buffer.BufferObject;
+import org.anddev.andengine.opengl.util.FastFloatBuffer;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class PolygonTextureRegionBuffer extends BufferObject {
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	protected final float mWidth;
+	protected final float mHeight;
+	protected final List<Vector2> mVertices;
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	public PolygonTextureRegionBuffer(
+			List<Vector2> vertices, 
+			float width,
+			float height,
+			int pDrawType,
+			boolean pManaged) {
+		super(vertices.size() * 2, pDrawType, pManaged);
+
+		this.mWidth = width;
+		this.mHeight = height;
+		this.mVertices = vertices;
+	}
+	
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	public synchronized void update() {
+		final int[] bufferData = this.mBufferData;
+
+		int i = 0;
+		for(Vector2 vertex: this.mVertices) {
+			bufferData[i++] = Float.floatToRawIntBits(vertex.x / this.mWidth);
+			bufferData[i++] = Float.floatToRawIntBits(vertex.y / this.mHeight);
+		}
+				
+		final FastFloatBuffer buffer = this.mFloatBuffer;
+		buffer.position(0);
+		buffer.put(bufferData);
+		buffer.position(0);
+
+		super.setHardwareBufferNeedsUpdate();
+	}
+
+
+
+	
+}

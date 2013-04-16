e7483eb47d2a3ad61286ec370fbbfbde95c16f2a
Andreas <andreas.wahlstrom@hotmail.com>
2013-04-16 13:53:19 +0200

    Poooooooower!

--- Twist/src/com/secondhand/model/PowerUp.java
+++ Twist/src/com/secondhand/model/PowerUp.java
@@ -6,11 +6,18 @@ import com.badlogic.gdx.math.Vector2;
 // class doesn't need to be abstract
 public abstract class PowerUp extends Entity {
 	
-	public PowerUp (Vector2 position, float radius) {
+	private Effect effect;
+	
+	public PowerUp (Vector2 position, float radius, Effect effect ) {
 		super(position,radius);
+		this.effect = effect;
 	}
 	
 	public enum Effect{
 		SPEED_UP, SCORE_UP, EAT_OBSTACLE, RANDOM_TELEPORT, SHIELD
+	}
+	
+	public Effect getEffect(){
+		return effect;
 	}
 }

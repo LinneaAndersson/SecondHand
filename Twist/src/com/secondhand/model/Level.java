935eed013ef81cecc4c0f882cd4cc9fcf230b04c
Johan Gerdin <johan.gerdin@hotmail.com>
2013-04-18 20:31:33 +0200

    removed import of CollisionContactListener in Level

--- Twist/src/com/secondhand/model/Level.java
+++ Twist/src/com/secondhand/model/Level.java
@@ -18,7 +18,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.secondhand.controller.CollisionContactListener;
 import com.secondhand.loader.TextureRegionLoader;
 import com.secondhand.math.PolygonUtil;
 import com.secondhand.model.PowerUp.Effect;

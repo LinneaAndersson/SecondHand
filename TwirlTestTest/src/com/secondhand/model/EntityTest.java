6ce86e52b28c84d7b5e11be99ce4d63dded6410e
Johan Gerdin <johan.gerdin@hotmail.com>
2013-04-18 20:12:23 +0200

    added more testing for Entity constructor

--- TwirlTestTest/src/com/secondhand/model/EntityTest.java
+++ TwirlTestTest/src/com/secondhand/model/EntityTest.java
@@ -8,11 +8,17 @@ import junit.framework.TestCase;
 public class EntityTest extends TestCase {
 
 	public void testConstructor() {
-		Vector2 v = new Vector2();
+		Vector2 vector = new Vector2();
 		float rad = 5;
-		Entity entityA = new Entity(v, rad, new Circle(0, 0, 5),true) {};
-		Entity entityB = new Entity(v, rad, new Circle(0, 0, 5),false) {};
+		Circle circle = new Circle(0, 0, 5);
+		Entity entityA = new Entity(vector, rad, circle,true) {};
+		Entity entityB = new Entity(vector, rad, circle,false) {};
 		
+		assertEquals(entityA.getRadius(), rad);
+		assertEquals(entityA.getShape(), circle);
+		
+		rad = 3;
+		entityA.setRadius(rad);
 		assertEquals(entityA.getRadius(), rad);
 	}
 	

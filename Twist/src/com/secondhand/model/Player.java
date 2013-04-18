166cdf7546a7df5fd8e10317a32f2aa9dacc2ec9
Andreas <andreas.wahlstrom@hotmail.com>
2013-04-18 15:01:26 +0200

    added check for powerUps in universe

--- Twist/src/com/secondhand/model/Player.java
+++ Twist/src/com/secondhand/model/Player.java
@@ -1,14 +1,16 @@
 package com.secondhand.model;
 
 import com.badlogic.gdx.math.Vector2;
+import com.secondhand.model.PowerUp.Effect;
 
 public class Player extends BlackHole {
 
 	private int score;
+	private Effect effect;
 
 	public Player(Vector2 position, float radius) {
 		super(position, radius);
-		
+		this.effect = Effect.NONE;
 		this.score = 0;
 	}
 
@@ -18,6 +20,14 @@ public class Player extends BlackHole {
 
 	public void increaseScore(int increase) {
 		score += increase;
+	}
+	
+	public Effect getEffect(){
+		return effect;
+	}
+	
+	public void setEffect(Effect effect){
+		this.effect = effect;
 	}
 
 }
--- Twist/src/com/secondhand/model/Universe.java
+++ Twist/src/com/secondhand/model/Universe.java
@@ -27,59 +27,92 @@ public final class Universe {
 		return instance;
 	}
 
-	public void checkCollision(Contact contact){
-		// if one or both is null, then we are dealing with a involving one or two non-entities
+	// Now there is alot of duplicated code in this method.
+	// Perhaps we could extract that code.
+	public void checkCollision(Contact contact) {
+		// if one or both is null, then we are dealing with a involving one or
+		// two non-entities
 		// (ie, a black hole collides with the wall),
-		//and we are not interested in handling such a collision
-		if(contact.getFixtureA().getBody().getUserData() == null ||
-				contact.getFixtureB().getBody().getUserData() == null) {
+		// and we are not interested in handling such a collision
+		if (contact.getFixtureA().getBody().getUserData() == null
+				|| contact.getFixtureB().getBody().getUserData() == null) {
 			return;
 		}
 
 		// now we know both the bodies are entities.
 
-		Entity entityA = (Entity)contact.getFixtureA().getBody().getUserData();
-		Entity entityB = (Entity)contact.getFixtureB().getBody().getUserData();
+		Entity entityA = (Entity) contact.getFixtureA().getBody().getUserData();
+		Entity entityB = (Entity) contact.getFixtureB().getBody().getUserData();
 
-		if(
-				entityA instanceof BlackHole && entityB instanceof Planet ||
-				entityB instanceof BlackHole && entityA instanceof Planet) {
+		if (entityA instanceof BlackHole && entityB instanceof Planet
+				|| entityB instanceof BlackHole && entityA instanceof Planet) {
 
 			BlackHole blackHole;
-			if(entityA instanceof BlackHole) {
-				blackHole = (BlackHole)entityA;
+			if (entityA instanceof BlackHole) {
+				blackHole = (BlackHole) entityA;
 			} else
-				blackHole = (BlackHole)entityB;
+				blackHole = (BlackHole) entityB;
 
 			Planet planet;
-			if(entityA instanceof Planet) {
-				planet = (Planet)entityA;
+			if (entityA instanceof Planet) {
+				planet = (Planet) entityA;
 			} else
-				planet = (Planet)entityB;
+				planet = (Planet) entityB;
 
-			if(blackHole.canEat(planet)) {
+			if (blackHole.canEat(planet)) {
 
 				// remove the planet from physics handling.
-				// TODO: this crashes the physics engine. Figure out how to fix. See:
+				// TODO: this crashes the physics engine. Figure out how to fix.
+				// See:
 				// http://stackoverflow.com/questions/10835349/crash-when-destroying-bodies
-				// and maybe also http://www.box2d.org/forum/viewtopic.php?f=8&t=5601&p=26070#p26070
-				/*final Body planetBody = planet.getBody();
-						PhysicsWorld pw = this.level.getPhysicsWorld();
-						pw.unregisterPhysicsConnector(
-								pw.getPhysicsConnectorManager().findPhysicsConnectorByShape(planet.getShape()));
-						pw.destroyBody(planetBody);*/
+				// and maybe also
+				// http://www.box2d.org/forum/viewtopic.php?f=8&t=5601&p=26070#p26070
+				/*
+				 * final Body planetBody = planet.getBody(); PhysicsWorld pw =
+				 * this.level.getPhysicsWorld(); pw.unregisterPhysicsConnector(
+				 * pw
+				 * .getPhysicsConnectorManager().findPhysicsConnectorByShape(planet
+				 * .getShape())); pw.destroyBody(planetBody);
+				 */
 
-				// deattach the planet from AndEngine
+				// Detach the planet from AndEngine
 				planet.getShape().detachSelf();
 
 				// increase the size of the black hole.
-				Shape blackHoleShape = blackHole.getBody().getFixtureList().get(0).getShape();
+				Shape blackHoleShape = blackHole.getBody().getFixtureList()
+						.get(0).getShape();
 				blackHole.increaseSize(planet.getRadius());
-				blackHoleShape.setRadius(blackHole.getRadius() / PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT);
+				blackHoleShape.setRadius(blackHole.getRadius()
+						/ PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT);
 
 				MyDebug.d("black hole should now eat planet.");
+
+			} else if (entityA instanceof Player && entityB instanceof PowerUp
+					|| entityB instanceof Player && entityA instanceof PowerUp) {
+
+				Player player;
+				if (entityA instanceof Player) {
+					player = (Player) entityA;
+				} else
+					player = (Player) entityB;
+
+				PowerUp power;
+				if (entityA instanceof PowerUp) {
+					power = (PowerUp) entityA;
+				} else
+					power = (PowerUp) entityB;
+
+				player.setEffect(power.getEffect());
+				power.getShape().detachSelf();
+
+				// now we need a way to have the power up take effect and decide
+				// a way to have the effect for a duration
+				// Also need to destroy the powerups body
+				// the effect should be visible on the players shape
+
 			}
 		}
+
 	}
 
 	public Level getLevel() {
@@ -98,6 +131,7 @@ public final class Universe {
 			nextLevel();
 
 		} else {
+
 			currentLevel.moveEntities(v);
 		}
 	}

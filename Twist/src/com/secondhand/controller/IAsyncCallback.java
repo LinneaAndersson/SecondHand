6830dfafe0443b21d6091444c93fe9eeabf14766
Eric Arnebäck <erkastina@hotmail.com>
2013-04-19 23:02:30 +0200

    Fixed PMD warnings in IAsyncCallback.

--- Twist/src/com/secondhand/controller/IAsyncCallback.java
+++ Twist/src/com/secondhand/controller/IAsyncCallback.java
@@ -4,7 +4,7 @@ package com.secondhand.controller;
  * Used in conjunction with AsyncTaskGameLoader to create the loading scene.
  */
 public interface IAsyncCallback {
-	public void work();
+	void work();
 
-	public void onWorkComplete();
+	void onWorkComplete();
 }

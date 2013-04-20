67b939cb8587b0a91a37d86dbce471e391525610
Eric Arnebäck <erkastina@hotmail.com>
2013-04-20 21:01:56 +0200

    Corrected PMD warnings in Enemy.

--- Twist/src/com/secondhand/model/Enemy.java
+++ Twist/src/com/secondhand/model/Enemy.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Enemy extends BlackHole {
 	
-	public Enemy (Vector2 vector, float radius) {
+	public Enemy (final Vector2 vector, final float radius) {
 		super(vector,radius);
 	}
 }

c2f74f3ad49cc9bf05d1f0a153d1ce55727f3f70
Johan Gerdin <johan.gerdin@hotmail.com>
2013-04-20 22:43:37 +0200

    rewrote EntityTest to make up for changes

--- TwirlTestTest/src/com/secondhand/model/EntityTest.java
+++ TwirlTestTest/src/com/secondhand/model/EntityTest.java
@@ -9,38 +9,31 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import junit.framework.TestCase;
 
 public class EntityTest extends TestCase {
-/*
+
 	public void testConstructor() {
-		Vector2 vector = new Vector2();
-		float rad = 5;
 		boolean isEdible = true;
 		// TODO add test for Shape parameter
-		Entity entity = new Entity(vector, rad, null, isEdible) {};
+		Entity entity = new Entity(null, isEdible) {
+			@Override
+			public float getArea() {
+				return 0;
+			}
+		};
 		
-		assertEquals(entity.getRadius(), rad);
 		assertEquals(entity.isEdible(), isEdible);
 	}
 	
-	public void testSetRadius() {
-		Vector2 vector = new Vector2();
-		float rad = 5;
-		boolean isEdible = true;
-		Entity entity = new Entity(vector, rad, null, isEdible) {};
-		
-		assertEquals(entity.getRadius(), rad);
-		rad = 3;
-		entity.setRadius(rad);
-		assertEquals(entity.getRadius(), rad);
-	}
-	
 	public void testSetBody() {
-		Vector2 vector = new Vector2();
-		float rad = 5;
 		boolean isEdible = true;
-		Entity entity = new Entity(vector, rad, null, isEdible) {};
+		Entity entity = new Entity(null, isEdible) {
+			@Override
+			public float getArea() {
+				return 0;
+			}
+		};
 		
 		Body testBody = new PhysicsWorld(new Vector2(),true).createBody(new BodyDef());
 		entity.setBody(testBody);
 		assertEquals(entity.getBody(), testBody);
-	}*/
+	}
 }

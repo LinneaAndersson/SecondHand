2c03a38809320cead59b2a68752d94d4aaf8f3da
Eric Arnebäck <erkastina@hotmail.com>
2013-04-22 14:09:27 +0200

    Refactored the Physics body destroying functionality into PhysicsDestroyer.

---
+++ Twist/src/com/secondhand/physics/PhysicsDestroyer.java
@@ -0,0 +1,72 @@
+package com.secondhand.physics;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.handler.IUpdateHandler;
+import org.anddev.andengine.entity.shape.IShape;
+import org.anddev.andengine.extension.physics.box2d.PhysicsConnector;
+import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
+
+import com.secondhand.debug.MyDebug;
+
+public final class PhysicsDestroyer {
+
+	// true when a body is being destroyed
+	private boolean killingInProcess;
+	
+	public PhysicsDestroyer() {
+		killingInProcess = false;
+	}
+	
+	/*
+	 * I found the following method on the AndEngine forum:
+	 * http://www.andengine.
+	 * org/forums/physics-box2d-extension/body-removal-crashing
+	 * -andengine-t9814.html (by RealMayo, a guy who knows lots of good stuff
+	 * bout engine. keep a look out for him if there is something you needs to
+	 * know) and it seems to work well. I thought i would get null value at the
+	 * third debug but it seems that the body still exist in the connector?
+	 */
+	public void destroy(final IShape mySprite, final Boolean bodyToo, final PhysicsWorld physicsWorld,
+			final Engine engine) {
+		final PhysicsWorld mPhysicsWorld = physicsWorld;
+
+		if (!killingInProcess) {
+			MyDebug.i("commence destruction");
+			killingInProcess = true;
+			final PhysicsConnector physicsConnector = mPhysicsWorld
+					.getPhysicsConnectorManager().findPhysicsConnectorByShape(
+							mySprite);
+			engine.registerUpdateHandler(new IUpdateHandler() {
+				@Override
+				public void onUpdate(final float pSecondsElapsed) {
+					engine.unregisterUpdateHandler(this);
+					engine.runOnUpdateThread(new Runnable() {
+						@Override
+						public void run() {
+							mPhysicsWorld
+									.unregisterPhysicsConnector(physicsConnector);
+							// myFixture.getBody().destroyFixture(myFixture);
+							// don't know if the above is needed
+							if (bodyToo) {
+								MyDebug.i(physicsConnector.getBody()
+										+ " will be destroyed");
+								mPhysicsWorld.destroyBody(physicsConnector
+										.getBody());
+							}
+							mySprite.detachSelf();
+							System.gc(); // NOPMD
+							killingInProcess = false;
+							MyDebug.i(physicsConnector.getBody()
+									+ " destruction complete");
+						}
+					});
+				}
+
+				@Override
+				public void reset() {
+				}
+			});
+		}
+	}
+
+}

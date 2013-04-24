7e89c6b2944bfd213c235831ae49043048a09150
Johan Gerdin <johan@localhost.localdomain>
2013-04-24 10:37:36 +0200

    player now uses a powerup

--- Twist/src/com/secondhand/model/Player.java
+++ Twist/src/com/secondhand/model/Player.java
@@ -3,17 +3,17 @@ package com.secondhand.model;
 import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
 
 import com.badlogic.gdx.math.Vector2;
-import com.secondhand.model.PowerUp.Effect;
+import com.secondhand.model.powerup.PowerUp;
 
 public class Player extends BlackHole {
 
 	private  int score;
-	private Effect effect;
 	private String name;
+	
+	private PowerUp powerUp;
 
 	public Player(final Vector2 position, final float radius, final PhysicsWorld physicsWorld) {
 		super(position, radius, physicsWorld, false);
-		this.effect = Effect.NONE;
 		this.score = 0;
 	}
 
@@ -25,12 +25,19 @@ public class Player extends BlackHole {
 		score += increase;
 	}
 	
-	public Effect getEffect(){
-		return effect;
+	public PowerUp getPowerUp(){
+		return powerUp;
 	}
 	
-	public void setEffect(final Effect effect){
-		this.effect = effect;
+	public void removePowerUp() {
+		this.powerUp.deactivateEffect(this);
+		this.powerUp = null;
+	}
+	
+	public void setPowerUp(final PowerUp powerUp){
+		this.powerUp = powerUp;
+		if (powerUp != null)
+			powerUp.activateEffect(this);			
 	}
 
 	public void setName(String name){

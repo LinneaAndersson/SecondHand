55b1c3b4e875160a25f332a149cdb4c0dfde6561
Johan Gerdin <johan.gerdin@hotmail.com>
2013-04-24 16:27:34 +0200

    altered PowerUpTest according to changes

--- TwirlTestTest/src/com/secondhand/model/PowerUpTest.java
+++ TwirlTestTest/src/com/secondhand/model/PowerUpTest.java
@@ -3,24 +3,26 @@ package com.secondhand.model;
 import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
 
 import com.badlogic.gdx.math.Vector2;
-import com.secondhand.model.PowerUp.Effect;
+import com.secondhand.model.powerup.PowerUp;
 
 import junit.framework.TestCase;
 
 public class PowerUpTest extends TestCase {
 	
 	public void testConstructor() {
-		final PhysicsWorld pw  =new PhysicsWorld(new Vector2(), true);
+		final PhysicsWorld pw = new PhysicsWorld(new Vector2(), true);
+		final Vector2 position = new Vector2(2,2);
 		
+		PowerUp pu1 = new PowerUp(position, pw, 5) {
+			@Override
+			public void activateEffect(Player player) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
 		
-		PowerUp pu1 = new PowerUp(new Vector2(10, 11),Effect.SCORE_UP, pw) {};
-		PowerUp pu2 = new PowerUp(new Vector2(),Effect.SPEED_UP, pw) {};
-		
-		assertEquals(pu1.getEffect(), Effect.SCORE_UP);
-		assertEquals(pu2.getEffect(), Effect.SPEED_UP);
-		
-		assertEquals(10f, pu1.getX());
-		assertEquals(11f, pu1.getY());
+		assertEquals(position.x, pu1.getX());
+		assertEquals(position.y, pu1.getY());
 		assertEquals(true, pu1.isEdible());
 	}	
 }

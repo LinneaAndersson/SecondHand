91fb4b343aad3ee5a7ae594cb42d2b443f3bd524
Johan Gerdin <johan@localhost.localdomain>
2013-04-24 10:31:38 +0200

    powerup in new package, enums expanded to classes

---
+++ Twist/src/com/secondhand/model/powerup/ScoreUp.java
@@ -0,0 +1,29 @@
+package com.secondhand.model.powerup;
+
+import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+
+import com.badlogic.gdx.math.Vector2;
+import com.secondhand.model.Player;
+
+public class ScoreUp extends PowerUp {
+
+	private final static float DURATION = 0;
+	
+	public ScoreUp(Vector2 position, TextureRegion texture,
+			PhysicsWorld physicsWorld) {
+		super(position, texture, physicsWorld, DURATION);
+
+	}
+
+	@Override
+	public void activateEffect(Player player) {
+		
+	}
+
+	@Override
+	public void deactivateEffect(Player player) {
+
+	}
+
+}
---
+++ Twist/src/com/secondhand/model/powerup/SpeedUp.java
@@ -0,0 +1,29 @@
+package com.secondhand.model.powerup;
+
+import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+
+import com.badlogic.gdx.math.Vector2;
+import com.secondhand.model.Player;
+
+public class SpeedUp extends PowerUp {
+
+	private final static float DURATION = 2;
+	
+	public SpeedUp(Vector2 position, TextureRegion texture,
+			PhysicsWorld physicsWorld) {
+		super(position, texture, physicsWorld, DURATION);
+		
+	}
+
+	@Override
+	public void activateEffect(Player player) {
+		player.getCircle().setColor(0, 0, 1f);
+	}
+
+	@Override
+	public void deactivateEffect(Player player) {
+
+	}
+
+}

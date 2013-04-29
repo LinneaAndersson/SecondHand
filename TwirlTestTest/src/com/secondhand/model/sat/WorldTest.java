c79313ddcf9ed1065f29bd0c3db4fc96c91d22d3
Eric Arnebäck <erkastina@hotmail.com>
2013-04-29 18:42:20 +0200

    Got SAT theorem to work for simple squares.

--- TwirlTestTest/src/com/secondhand/model/sat/WorldTest.java
+++ TwirlTestTest/src/com/secondhand/model/sat/WorldTest.java
@@ -23,5 +23,21 @@ public class WorldTest extends TestCase {
 		World.Polygon polygon = new World.Polygon(new Vector2(20,20), edges);
 		
 		assertTrue(world.isUnoccupied(polygon));
+		
+		List<Vector2> edges2 = new ArrayList();
+		edges.add(new Vector2(0,0));
+		edges.add(new Vector2(20,0));
+		edges.add(new Vector2(20,20));
+		edges.add(new Vector2(0,20));
+		World.Polygon polygon2 = new World.Polygon(new Vector2(10,10), edges);
+		
+		assertTrue(world.isUnoccupied(polygon2));
+		
+		world.addToWorld(polygon);
+		
+		
+		assertFalse(world.isUnoccupied(polygon2));
+		
+		
 	}
 }

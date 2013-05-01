98b00a9614c73adf88f8877e09687a7777020fe9
Johan Gerdin <johan.gerdin@hotmail.com>
2013-05-01 22:30:54 +0200

    testActivateEffect added

---
+++ TwirlTestTest/src/com/secondhand/model/powerup/TestDoubleScore.java
@@ -0,0 +1,22 @@
+package com.secondhand.model.powerup;
+
+import com.badlogic.gdx.math.Vector2;
+import com.secondhand.model.GameWorld;
+import com.secondhand.model.Player;
+
+import junit.framework.TestCase;
+
+public class TestDoubleScore extends TestCase {
+
+	public void testActivateEffect() {
+		GameWorld world = new GameWorld();
+		Player player = new Player(new Vector2(), 10, world, 1, 0);
+		DoubleScore powerUp = new DoubleScore(new Vector2(), world);
+		
+		int inc = 10;
+		powerUp.activateEffect(player);
+		player.increaseScore(inc);
+		assertEquals(inc*2, player.getScore());
+	}
+	
+}

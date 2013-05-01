3b221437d23d4001bcf94df1aaffe1f0582e5b82
Johan Gerdin <johan.gerdin@hotmail.com>
2013-05-01 22:27:41 +0200

    testActivateEffect added

--- TwirlTestTest/src/com/secondhand/model/powerup/ShieldTest.java
+++ TwirlTestTest/src/com/secondhand/model/powerup/ShieldTest.java
@@ -1,7 +1,20 @@
 package com.secondhand.model.powerup;
 
+import com.badlogic.gdx.math.Vector2;
+import com.secondhand.model.GameWorld;
+import com.secondhand.model.Player;
+
 import junit.framework.TestCase;
 
 public class ShieldTest extends TestCase {
 
+	public void testActivateEffect() {
+		GameWorld world = new GameWorld();
+		Player player = new Player(new Vector2(), 10, world);
+		Shield powerUp = new Shield(new Vector2(), world);
+		
+		assertEquals(true, player.isEdible());
+		powerUp.activateEffect(player);
+		assertEquals(false, player.isEdible());
+	}
 }

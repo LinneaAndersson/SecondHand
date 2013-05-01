ee08795ac39eb6075e0846e7ac89455fb6c78038
Johan Gerdin <johan.gerdin@hotmail.com>
2013-05-01 22:27:38 +0200

    testActivateEffect added

--- TwirlTestTest/src/com/secondhand/model/powerup/ExtraLifeTest.java
+++ TwirlTestTest/src/com/secondhand/model/powerup/ExtraLifeTest.java
@@ -1,7 +1,22 @@
 package com.secondhand.model.powerup;
 
+import com.badlogic.gdx.math.Vector2;
+import com.secondhand.model.GameWorld;
+import com.secondhand.model.Player;
+
 import junit.framework.TestCase;
 
 public class ExtraLifeTest extends TestCase {
 
+	public void testActivateEffect() {
+		GameWorld world = new GameWorld();
+		int lives = 1;
+		Player player = new Player(new Vector2(), 10, world, lives, 0);
+		ExtraLife powerUp = new ExtraLife(new Vector2(), world);
+		
+		assertEquals(lives, player.getLives());
+		powerUp.activateEffect(player);
+		assertEquals(lives+1, player.getLives());
+	}
+	
 }

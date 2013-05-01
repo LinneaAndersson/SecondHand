91b1f690b0027211554e8c2517c7b5c9079ed174
Eric Arnebäck <erkastina@hotmail.com>
2013-05-01 08:47:11 +0200

    Made the high score scene look better.

--- Twist/src/com/secondhand/scene/HighScoreScene.java
+++ Twist/src/com/secondhand/scene/HighScoreScene.java
@@ -1,8 +1,5 @@
 package com.secondhand.scene;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 import org.anddev.andengine.engine.Engine;
 import org.anddev.andengine.entity.text.Text;
 import org.anddev.andengine.opengl.font.Font;
@@ -19,6 +16,17 @@ import com.secondhand.resource.LocalizationStrings;
 
 public class HighScoreScene extends GameScene {
 
+	// position of the high score table
+	private float x;
+	private float y;
+	
+	// index so far in the high score table.
+	private int i = 0;
+
+	private float timer;
+	
+	public static final float SECONDS_PER_HIGH_SCORE_ENTRY = 0.5f;
+	
 	public HighScoreScene(final Engine engine, final Context context) {
 		super(engine, context);
 	}
@@ -36,27 +44,51 @@ public class HighScoreScene extends GameScene {
 
 		this.attachChild(highScore);
 
+		x = this.smoothCamera.getWidth() / 2.0f-120;
+		y = this.smoothCamera.getHeight() / 2.0f-20;
+		
+		
+		timer = 0;
+		i = 0;
+	}
 	
-		//We know which position the name will begin and where the score will end.
-		final float x = this.smoothCamera.getWidth() / 2.0f-120;
-		final 	float y = this.smoothCamera.getHeight() / 2.0f-20;
+	@Override
+	protected void onManagedUpdate(final float pSecondsElapsed) {
+		super.onManagedUpdate(pSecondsElapsed);
+		
+		timer += pSecondsElapsed;
 
-			for(int i = 0; i < HighScoreList.getInstance().getHighScoreList().size(); ++i) {
-				
-				final Entry entry =  HighScoreList.getInstance().getHighScoreList().get(i);
-
-				final Text playerScoreName = new Text(x,y,Fonts.getInstance().menuItemFont, (i+1) + ". " + entry.name);
-				final Text playerScore = new Text(x, y,
-						Fonts.getInstance().menuItemFont, entry.score + "");
-				// increase the y-axis for every player. Max 5 players! 
-				playerScoreName.setPosition(x, (int) (y * (0.35 + (i+1) * 0.3)));
-				// x-constant because the name cannot be bigger than "220"(e.g x+220)
-				playerScore.setPosition(x+220, (int) (y * (0.35 + (i+1) * 0.3)));
-
-				this.attachChild(playerScore);
-				this.attachChild(playerScoreName);
-			}
+		MyDebug.d("second: " + pSecondsElapsed);
+		
+		MyDebug.d("timer: " + timer);
+		
+		if(timer > SECONDS_PER_HIGH_SCORE_ENTRY) {
 			
+			timer = 0;
+			
+			if(i == HighScoreList.getInstance().getHighScoreList().size()) {
+				this.isLoaded = false;
+				super.setScene(AllScenes.MAIN_MENU_SCENE);
+				return;
+			}
+
+			final Entry entry =  HighScoreList.getInstance().getHighScoreList().get(i);
+
+			final Text playerScoreName = new Text(x,y,Fonts.getInstance().menuItemFont, (i+1) + ". " + entry.name);
+			final Text playerScore = new Text(x, y,
+					Fonts.getInstance().menuItemFont, entry.score + "");
+			// increase the y-axis for every player. Max 5 players! 
+			playerScoreName.setPosition(x, (int) (y * (0.35 + (i+1) * 0.3)));
+			// x-constant because the name cannot be bigger than "220"(e.g x+220)
+			playerScore.setPosition(x+220, (int) (y * (0.35 + (i+1) * 0.3)));
+
+			this.attachChild(playerScore);
+			this.attachChild(playerScoreName);
+
+			// next entry in the next showing. 
+			i++;
+		}
+
 	}
 
 	@Override

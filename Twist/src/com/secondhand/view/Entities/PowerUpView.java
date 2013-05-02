ed85b6a32497d13a0a56ef53c282aea94e7f5fa5
Johan Gerdin <johan.gerdin@hotmail.com>
2013-05-02 15:23:51 +0200

    moved getTimer to PowerUpView

--- Twist/src/com/secondhand/view/Entities/PowerUpView.java
+++ Twist/src/com/secondhand/view/Entities/PowerUpView.java
@@ -1,5 +1,22 @@
 package com.secondhand.view.Entities;
 
+import org.anddev.andengine.engine.handler.timer.ITimerCallback;
+import org.anddev.andengine.engine.handler.timer.TimerHandler;
+
+import com.secondhand.model.Player;
+import com.secondhand.model.powerup.PowerUp;
+
 public class PowerUpView implements IEntityView{
 
+	public TimerHandler getTimer(final Player player, final PowerUp powerUp, final float duration) {
+		return new TimerHandler(duration, new ITimerCallback() {
+			private Player user = player; 
+			@Override
+			public void onTimePassed(final TimerHandler pTimerHandler) {
+				if (user.getPowerUps().contains(powerUp))
+					user.removePowerUp(powerUp);
+			}
+		});
+	}
+	
 }

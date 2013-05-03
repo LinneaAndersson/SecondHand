1ca828c85901653f19bc66bd87874c4e37c88666
LinneaAndersson <Linneamaria10@gmail.com>
2013-05-03 19:39:19 +0200

    removed unnecessery comment

--- Twist/src/com/secondhand/model/CircleEntity.java
+++ Twist/src/com/secondhand/model/CircleEntity.java
@@ -1,36 +1,15 @@
 package com.secondhand.model;
 
-import org.anddev.andengine.extension.physics.box2d.PhysicsFactory;
-import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
-
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.secondhand.view.opengl.Circle;
 
 public abstract class CircleEntity extends Entity {
 	
 	protected final Circle circle;
-	private final Body mBody;
-	
-	//public static Body createCircleBody(final PhysicsWorld pPhysicsWorld, final float pCenterX, final float pCenterY, final float pRadius, final float pRotation, final BodyType pBodyType, final FixtureDef pFixtureDef) {
-
-	
-	/*protected Body createNewCircleBody(final Circle circle) {
-		 return physics.createType(circle,this);
-		  
-		 
-		
-		return PhysicsFactory.createCircleBody(physicsWorld,
-				circle.getX(), circle.getY(), circle.getRadius(),circle.getRotation(), BodyType.DynamicBody, fixtureDef);
-	
-	 }*/
 	
 	public CircleEntity(final Circle circle, final boolean isEdible, final GameWorld level) {
 		
 		  super(circle,isEdible,level);
-		 mBody=physics.createType(circle,this);
-		 this.setBody(mBody);
+		 this.setBody(physics.createType(circle,this));
 		
 		this.circle = circle;
 	}

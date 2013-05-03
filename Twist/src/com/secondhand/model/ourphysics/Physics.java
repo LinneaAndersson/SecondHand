fdda9c912b2004a7d7b63ca79b2c9365a5a62ac8
LinneaAndersson <Linneamaria10@gmail.com>
2013-05-03 12:30:01 +0200

    removed GameWorldBounds, fixed the problem in physics

--- Twist/src/com/secondhand/model/ourphysics/Physics.java
+++ Twist/src/com/secondhand/model/ourphysics/Physics.java
@@ -1,6 +1,8 @@
 package com.secondhand.model.ourphysics;
 
+import org.anddev.andengine.entity.primitive.Rectangle;
 import org.anddev.andengine.entity.shape.IShape;
+import org.anddev.andengine.entity.shape.Shape;
 import org.anddev.andengine.extension.physics.box2d.PhysicsConnector;
 import org.anddev.andengine.extension.physics.box2d.PhysicsFactory;
 import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
@@ -42,19 +44,29 @@ public class Physics implements IPhysics {
 	// world bounds:
 	// we do not do this using registerEntity, because these bodies are
 	// static.
-	public void setWorldBounds(final IShape[] shape) {
+	public void setWorldBounds(final int levelWidth, final int levelHeight) {
 		bodies = new Body[4];
+		worldBounds = new Shape[4];
+		bodies = new Body[4];
+
+		// put some invisible, static rectangles that keep the player within the
+		// world bounds:
+
+		worldBounds[0] = new Rectangle(0, levelHeight - 2, levelWidth, 2);
+		worldBounds[1] = new Rectangle(0, 0, levelWidth, 2);
+		worldBounds[2] = new Rectangle(0, 0, 2, levelHeight);
+		worldBounds[3] = new Rectangle(levelWidth - 2, 0, 2, levelHeight);
 
 		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0,
 				0.5f, 0.5f);
 
-		bodies[0] = PhysicsFactory.createBoxBody(physicsWorld, shape[0],
+		bodies[0] = PhysicsFactory.createBoxBody(physicsWorld, worldBounds[0],
 				BodyType.StaticBody, wallFixtureDef);
-		bodies[1] = PhysicsFactory.createBoxBody(physicsWorld, shape[1],
+		bodies[1] = PhysicsFactory.createBoxBody(physicsWorld, worldBounds[1],
 				BodyType.StaticBody, wallFixtureDef);
-		bodies[2] = PhysicsFactory.createBoxBody(physicsWorld, shape[2],
+		bodies[2] = PhysicsFactory.createBoxBody(physicsWorld, worldBounds[2],
 				BodyType.StaticBody, wallFixtureDef);
-		bodies[3] = PhysicsFactory.createBoxBody(physicsWorld, shape[3],
+		bodies[3] = PhysicsFactory.createBoxBody(physicsWorld, worldBounds[3],
 				BodyType.StaticBody, wallFixtureDef);
 	}
 

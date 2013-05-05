9a4c8f8a641d508233e4e39ee96eb87d6c306844
Johan Gerdin <johan.gerdin@hotmail.com>
2013-05-05 18:02:46 +0200

    removed code, will be used later

--- Twist/src/com/secondhand/view/entities/PlayerView.java
+++ Twist/src/com/secondhand/view/entities/PlayerView.java
@@ -25,50 +25,6 @@ public class PlayerView extends BlackHoleView {
 	public PlayerView(final Player player){
 		super(player);
 	}
-/*
-	public PlayerView(final Engine engine, final GameWorld gameWorld) {
-		//TODO: remove this later.
-		super(gameWorld.getPlayer());
-		this.engine = engine;
-		this.gameWorld = gameWorld;
-
-		this.gameWorld.getPlayer().addListener(this);
-	}
-
-	public void playerMoveAnimation(final Vector2 touch) {
-
-		final Player player = gameWorld.getPlayer();
-
-		final TextureRegion particleTexture = TextureRegionLoader.getInstance().loadTextureRegion("particle.png", 16, 16);
-
-		final Vector2 surfacePosition = getRelativeSurfacePosition(player,
-				touch);
-
-		final PointParticleEmitter movementEmitter = new PointParticleEmitter(
-				surfacePosition.x, surfacePosition.y);
-		final ParticleSystem particleSystem = new ParticleSystem(
-				movementEmitter, 60, 60, 10, particleTexture);
-
-		// TODO: How to access GamePlayScene?
-		// attachChild(particleSystem);
-		
-		final float duration = 2;
-		engine.registerUpdateHandler(new TimerHandler(duration,
-				new ITimerCallback() {
-			@Override
-			public void onTimePassed(final TimerHandler pTimerHandler) {
-				// GamePlayScene.this.detachChild(particleSystem); // Read TODO above
-			}
-		}));
-	}
-
-	// Calculate the surface position of object relative to given position
-	public Vector2 getRelativeSurfacePosition(final Entity object, final Vector2 position) {
-		final Vector2 surfacePosition = new Vector2(object.getCenterX() - position.x,	
-											  object.getCenterY() - position.y); // Vector from object position to given position
-		surfacePosition.mul(object.getRadius() / surfacePosition.len()); // Length of new vector increased/decreased to length of radius
-		return surfacePosition;
-	}*/
 
 	@Override
 	public void propertyChange(final PropertyChangeEvent event) {

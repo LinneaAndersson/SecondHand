7b4b8e9fe1348bfd7dbd4e3a0ef693367a949da4
LinneaAndersson <Linneamaria10@gmail.com>
2013-05-05 16:53:03 +0200

    created enemyView

---
+++ Twist/src/com/secondhand/view/entities/EnemyView.java
@@ -0,0 +1,21 @@
+package com.secondhand.view.entities;
+
+import java.beans.PropertyChangeEvent;
+
+import com.secondhand.model.Enemy;
+import com.secondhand.model.Entity;
+
+public class EnemyView extends EntityView{
+
+	public EnemyView(Enemy enemy) {
+		super(enemy);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

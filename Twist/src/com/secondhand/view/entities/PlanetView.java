d6574ff78c74f565ca97fface18767774c9dcf3f
Johan Gerdin <johan.gerdin@hotmail.com>
2013-05-05 18:25:20 +0200

    removed comments

--- Twist/src/com/secondhand/view/entities/PlanetView.java
+++ Twist/src/com/secondhand/view/entities/PlanetView.java
@@ -9,14 +9,11 @@ public class PlanetView extends EntityView {
 
 
 	public PlanetView(Entity entity) {
-		//Just for now.
 		super(entity, new Circle(0,0,0));
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void propertyChange(PropertyChangeEvent event) {
-		// TODO Auto-generated method stub
 		
 	}
 

31dc0e265e7be3597c6330a0f136e4b48c1d881d
Eric Arnebäck <erkastina@hotmail.com>
2013-05-07 12:15:05 +0200

    Added RectangleView.

--- Twist/src/com/secondhand/model/RectangleEntity.java
+++ Twist/src/com/secondhand/model/RectangleEntity.java
@@ -1,11 +1,25 @@
 package com.secondhand.model;
 
+import java.util.List;
+
 import org.anddev.andengine.entity.shape.RectangularShape;
 
 
 public abstract class RectangleEntity extends Entity {
+		
+
+	private final Vector2 position;
+	private final float width;
+	private final float height;
 	
-	protected RectangularShape rectangle;
+	public PolygonEntity(final Vector2 position,  final List<Vector2> polygon, 
+			final boolean isEdible, final GameWorld level) {
+		super(isEdible, level);
+		
+		this.position = position;
+		this.polygon = polygon;
+	}*/
+	
 	
 	public RectangleEntity(final RectangularShape rectangle, final boolean isEdible, final GameWorld level) {
 		super(rectangle, isEdible, level);

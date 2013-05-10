cd7516c86e01019ac375ec672d48f9fc90c9369d
LinneaAndersson <Linneamaria10@gmail.com>
2013-05-10 12:19:48 +0200

    debugged

--- Twist/src/com/secondhand/view/physics/MyPhysicsEntity.java
+++ Twist/src/com/secondhand/view/physics/MyPhysicsEntity.java
@@ -10,7 +10,6 @@ import org.anddev.andengine.extension.physics.box2d.util.constants.PhysicsConsta
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.secondhand.debug.MyDebug;
-import com.secondhand.model.BlackHole;
 import com.secondhand.model.Entity;
 import com.secondhand.model.IPhysicsEntity;
 import com.secondhand.view.opengl.Circle;

39c3c76abd0e5e1ce3e962ae6c8d49bdedc06e73
Eric Arnebäck <erkastina@hotmail.com>
2013-05-12 08:44:40 +0200

    working on better level transitions.

---
+++ Twist/src/com/secondhand/view/scene/LoadingSceneOverlay.java
@@ -0,0 +1,32 @@
+package com.secondhand.view.scene;
+
+
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.entity.Entity;
+import org.anddev.andengine.entity.primitive.Rectangle;
+
+import com.secondhand.view.entity.LoadingTextView;
+
+// a transparent overlay that can be used as a loading scene. 
+public class LoadingSceneOverlay extends Entity{
+
+	public LoadingSceneOverlay(final Camera camera) {
+		super();
+		this.loadingTextView = new LoadingTextView(camera);
+		 
+		// add loading text
+		this.attachChild(this.loadingTextView);
+		
+		// transparent rectangle overlay
+		final Rectangle overlay = new Rectangle(camera.getMinX(),camera.getMinY(), camera.getMaxX(), camera.getMaxY());
+		overlay.setColor(0, 0, 0, 0.5f);
+		this.attachChild(overlay);
+	}
+	
+	private  final LoadingTextView loadingTextView;
+	
+	public LoadingTextView getLoadingTextView() {
+		return this.loadingTextView;
+	}
+	
+}

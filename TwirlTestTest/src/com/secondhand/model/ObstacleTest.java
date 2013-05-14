09671db0e37df2140d1200dfa655621c4166e012
Johan Gerdin <johan.gerdin@hotmail.com>
2013-05-14 19:30:08 +0200

    started working on ObstacleTest

--- TwirlTestTest/src/com/secondhand/model/ObstacleTest.java
+++ TwirlTestTest/src/com/secondhand/model/ObstacleTest.java
@@ -1,44 +1,19 @@
 package com.secondhand.model;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import junit.framework.TestCase;
 
-import com.badlogic.gdx.math.Vector2;
-import com.secondhand.view.opengl.Polygon;
-/*
+import com.secondhand.model.entity.Obstacle;
+import com.secondhand.model.physics.Vector2;
+
 public class ObstacleTest extends TestCase{
 
 	public void testConstructor() {
+		Vector2 position = new Vector2();
+		ArrayList<Vector2> polygon = new ArrayList<Vector2>();
 		
-		final GameWorld gW = new GameWorld();		
-		
-		Vector2 pos = new Vector2(2f, 4f);
-		List<Vector2> points = new ArrayList<Vector2>();
-		
-		// simple triangle.
-		points.add(new Vector2(0, 0));
-		points.add(new Vector2(10, 0));
-		points.add(new Vector2(0, 10));
-		
-		Polygon p = new Polygon(pos, points);
-		
-		Obstacle obstacle = new Obstacle(p, gW);
-		
-		assertEquals(pos.x, obstacle.getX());
-		assertEquals(pos.y, obstacle.getY());
-		assertEquals(false, obstacle.isEdible());
-		
-		assertEquals(0f, obstacle.getPolygon().getPolygon().get(0).x);
-		assertEquals(0f, obstacle.getPolygon().getPolygon().get(0).y);
-		
-		assertEquals(10f, obstacle.getPolygon().getPolygon().get(1).x);
-		assertEquals(0f, obstacle.getPolygon().getPolygon().get(1).y);
-		
-		assertEquals(0f, obstacle.getPolygon().getPolygon().get(2).x);
-		assertEquals(10f, obstacle.getPolygon().getPolygon().get(2).y);
-		
+		Obstacle obstacle = new Obstacle(position, polygon);		
 	}
 	
-}*/
+}

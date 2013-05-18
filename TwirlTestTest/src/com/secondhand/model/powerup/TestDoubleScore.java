3a40478f1b65ed0b45a7f046e09b418107760c0d
Eric Arnebäck <erkastina@hotmail.com>
2013-05-18 11:15:28 +0200

    Tested DoubleScore

--- TwirlTestTest/src/com/secondhand/model/powerup/TestDoubleScore.java
+++ TwirlTestTest/src/com/secondhand/model/powerup/TestDoubleScore.java
@@ -1,22 +1,36 @@
 package com.secondhand.model.powerup;
 
-import com.badlogic.gdx.math.Vector2;
-import com.secondhand.model.entity.GameWorld;
+
 import com.secondhand.model.entity.Player;
+import com.secondhand.model.physics.Vector2;
+import com.secondhand.model.resource.PowerUpType;
 
 import junit.framework.TestCase;
 
 public class TestDoubleScore extends TestCase {
 
-/*	public void testActivateEffect() {
-		GameWorld world = new GameWorld();
-		Player player = new Player(new Vector2(), 10, world, 1, 0);
-		DoubleScore powerUp = new DoubleScore(new Vector2(), world);
+	public void testEffet() {
+		Player player = new Player(new Vector2(), 10f, 3, 0, 100);
+		DoubleScore powerup = new DoubleScore(new Vector2());
 		
-		int inc = 10;
-		powerUp.activateEffect(player);
-		player.increaseScore(inc);
-		assertEquals(inc*player.getScoreMultiplier(), player.getScore());
-	}*/
+		assertEquals(10, powerup.getDuration(), 0.001);
+		assertEquals(5, DoubleScore.getFrequency());	
+		
+		assertEquals(1, powerup.getR(), 0.001);	
+		assertEquals(1, powerup.getG(), 0.001);	
+		assertEquals(0, powerup.getB(), 0.001);	
+		
+		assertEquals(PowerUpType.DOUBLE_SCORE, powerup.getPowerUpType());	
+		
+		powerup.activateEffect(player);
+		assertEquals(2, player.getScoreMultiplier(), 0.001);
+		
+		powerup.deactivateEffect(player, true);
+		assertEquals(2, player.getScoreMultiplier(), 0.001);
+		
+		powerup.deactivateEffect(player, false);
+		assertEquals(1, player.getScoreMultiplier(), 0.001);
+		
+	}
 	
 }

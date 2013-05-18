02a7d3aa5819d185ba14451a69bcd35a961719ad
Eric Arnebäck <erkastina@hotmail.com>
2013-05-18 08:58:55 +0200

    Tested constructor of Player.

--- TwirlTestTest/src/com/secondhand/model/PlayerTest.java
+++ TwirlTestTest/src/com/secondhand/model/PlayerTest.java
@@ -1,22 +1,24 @@
 package com.secondhand.model;
 
+import com.secondhand.model.entity.Player;
+import com.secondhand.model.physics.Vector2;
+
 import junit.framework.TestCase;
 
 public class PlayerTest extends TestCase{
-/*
+
 	public void testConstructor() {
 		
 		
-		Vector2 pos = new Vector2(2f, 4f);
-		float rad = 3.2f;
+		Player player = new Player(new Vector2(), 10, 3, // lives
+				100, // starting score
+				200); // maxsize
 		
-		Player player = new Player(pos, rad, gW);
-		
-		assertEquals(rad, player.getRadius());
-		assertEquals(pos.x, player.getX());
-		assertEquals(pos.y, player.getY());
-		assertEquals(true, player.isEdible());
-		
-	}*/
+		assertEquals(200, player.getMaxSize());
+		assertEquals(false, player.isMirroredMovement());
+		assertEquals(1, player.getSpeedMultiplier(), 0.0001f);
+		assertEquals(1, player.getScoreMultiplier(), 0.0001f);
+		assertEquals(3, player.getLives());
+	}
 
 }

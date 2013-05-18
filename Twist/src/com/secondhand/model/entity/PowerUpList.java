9b34e22f43b6b25bec23daa50335ab7bf5d5c166
Eric Arnebäck <erkastina@hotmail.com>
2013-05-18 09:11:55 +0200

    Got rid of logging.

--- Twist/src/com/secondhand/model/entity/PowerUpList.java
+++ Twist/src/com/secondhand/model/entity/PowerUpList.java
@@ -4,8 +4,6 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 
-import com.secondhand.debug.MyDebug;
-
 public final class PowerUpList extends ArrayList<IPowerUp> {
 
 	private static final long serialVersionUID = 1L;
@@ -34,11 +32,8 @@ public final class PowerUpList extends ArrayList<IPowerUp> {
 		// empty when you remove
 		// last PowerUp
 		final IPowerUp powerUp = (IPowerUp) object;
-		MyDebug.d("deactivating powerup:" + powerUp + " another:" + hasAnother(powerUp));
-		
 		// out of powerups? then reset player color altogether. 
 		if(this.size() == 0) {
-			MyDebug.d("restoring player color");
 			powerUp.resetPlayerColor(player);
 		}
 		powerUp.deactivateEffect(player, hasAnother(powerUp));

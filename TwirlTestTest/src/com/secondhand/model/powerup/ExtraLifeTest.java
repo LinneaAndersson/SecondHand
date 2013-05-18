9cf07dc243b8ab03c4ef362cfba5df6b4ce058fe
Eric Arnebäck <erkastina@hotmail.com>
2013-05-18 11:19:12 +0200

    Tested ExtraLife.

--- TwirlTestTest/src/com/secondhand/model/powerup/ExtraLifeTest.java
+++ TwirlTestTest/src/com/secondhand/model/powerup/ExtraLifeTest.java
@@ -1,22 +1,27 @@
 package com.secondhand.model.powerup;
 
-import com.badlogic.gdx.math.Vector2;
-import com.secondhand.model.entity.GameWorld;
 import com.secondhand.model.entity.Player;
+import com.secondhand.model.physics.Vector2;
+import com.secondhand.model.resource.PowerUpType;
 
 import junit.framework.TestCase;
 
 public class ExtraLifeTest extends TestCase {
-/*
-	public void testActivateEffect() {
-		GameWorld world = new GameWorld();
-		int lives = 1;
-		Player player = new Player(new Vector2(), 10, world, lives, 0);
-		ExtraLife powerUp = new ExtraLife(new Vector2(), world);
+
+	public void testEffet() {
+		Player player = new Player(new Vector2(), 10f, 3, 0, 100);
+		ExtraLife powerup = new ExtraLife(new Vector2());
 		
-		assertEquals(lives, player.getLives());
-		powerUp.activateEffect(player);
-		assertEquals(lives+1, player.getLives());
+		assertEquals(0, powerup.getDuration(), 0.001);
+		assertEquals(1, ExtraLife.getFrequency());	
+		assertEquals("1UP", powerup.getText());	
+		
+		assertEquals(PowerUpType.EXTRA_LIFE, powerup.getPowerUpType());	
+		
+		powerup.activateEffect(player);
+		
+		assertEquals(4, player.getLives());
+		
 	}
-	*/
+	
 }

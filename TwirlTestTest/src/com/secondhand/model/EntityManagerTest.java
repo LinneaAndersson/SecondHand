44fb0e8e4fadc94f3a9c366998896759419f382f
Andreas <andreas.wahlstrom@hotmail.com>
2013-05-19 13:39:06 +0200

    working on tests

---
+++ TwirlTestTest/src/com/secondhand/model/EntityManagerTest.java
@@ -0,0 +1,11 @@
+package com.secondhand.model;
+
+import junit.framework.TestCase;
+
+public class EntityManagerTest extends TestCase{
+	
+	public void testConstructor(){
+	
+	}
+
+}
--- TwirlTestTest/src/com/secondhand/model/GameWorldTest.java
+++ TwirlTestTest/src/com/secondhand/model/GameWorldTest.java
@@ -1,10 +1,28 @@
 package com.secondhand.model;
 
+import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
+
 import junit.framework.TestCase;
+
+import com.secondhand.model.entity.GameWorld;
+import com.secondhand.model.physics.IPhysicsWorld;
+import com.secondhand.model.physics.Vector2;
+import com.secondhand.view.physics.MyPhysicsWorld;
 
 public class GameWorldTest extends TestCase {
 
-	public void testConstructor(){
-
+	public void testConstructor() {
+		/*
+		 * int lives = 3; int levelNumber = 1; int score = 100;
+		 * 
+		 * IPhysicsWorld physics = new MyPhysicsWorld(new PhysicsWorld(new
+		 * com.badlogic.gdx.math.Vector2(), true)); GameWorld gWorld = new
+		 * GameWorld(physics,levelNumber, lives, score);
+		 * 
+		 * assertEquals(lives, gWorld.getLevelNumber()); assertEquals(score,
+		 * gWorld.getPlayer().getScore()); assertEquals(lives,
+		 * gWorld.getPlayer().getLives()); assertEquals(physics,
+		 * gWorld.getPhysics());
+		 */
 	}
 }

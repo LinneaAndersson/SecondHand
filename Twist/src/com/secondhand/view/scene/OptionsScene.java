0c721e37ef2ac1eb669d91fb27852b1937712ec7
LinneaAndersson <Linneamaria10@gmail.com>
2013-05-21 13:15:12 +0200

    fixed the headline for instructions and options, so that all the subcathegories to the menu has the same layout

--- Twist/src/com/secondhand/view/scene/OptionsScene.java
+++ Twist/src/com/secondhand/view/scene/OptionsScene.java
@@ -4,7 +4,11 @@ import org.anddev.andengine.engine.Engine;
 import org.anddev.andengine.entity.scene.menu.MenuScene;
 import org.anddev.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
 import org.anddev.andengine.entity.scene.menu.item.IMenuItem;
+import org.anddev.andengine.entity.text.Text;
+import org.anddev.andengine.opengl.font.Font;
+import org.anddev.andengine.util.HorizontalAlign;
 
+import com.secondhand.view.resource.Fonts;
 import com.secondhand.view.resource.LocalizationStrings;
 import com.secondhand.view.scene.GameMenuScene.MenuItem;
 
@@ -23,7 +27,14 @@ public class OptionsScene extends GameMenuScene {
 	public void loadScene() {
 		super.loadScene();
 		
-		final int menuStartX = layoutHeadline("Options");
+		final Font mFont = Fonts.getInstance().menuItemFont;
+		
+		final Text options = new Text(100, 60, mFont, LocalizationStrings
+				.getInstance().getLocalizedString("menu_options"),
+				HorizontalAlign.CENTER);
+		options.setScale(1.5f);
+
+		this.attachChild(options);
 	
 	new MenuItem(MIRRORED_MOVEMENT_FALSE, LocalizationStrings.getInstance().getLocalizedString("menu_new_game"));
 	new MenuItem(MIRRORED_MOVEMENT_TRUE, LocalizationStrings.getInstance().getLocalizedString("menu_new_game"));

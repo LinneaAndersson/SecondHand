bbba06f33c7e9c120bbc303edf01bfbd66caa2ff
LinneaAndersson <Linneamaria10@gmail.com>
2013-05-21 11:48:01 +0200

    crated instructionScene and OptionScene

---
+++ Twist/src/com/secondhand/view/scene/OptionsScene.java
@@ -0,0 +1,30 @@
+package com.secondhand.view.scene;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.menu.MenuScene;
+import org.anddev.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
+import org.anddev.andengine.entity.scene.menu.item.IMenuItem;
+
+import android.content.Context;
+
+public class OptionsScene extends GameMenuScene implements IOnMenuItemClickListener {
+
+	public OptionsScene(Engine engine, Context context) {
+		super(engine, context);
+
+	}
+
+	@Override
+	public boolean onMenuItemClicked(MenuScene pMenuScene, IMenuItem pMenuItem,
+			float pMenuItemLocalX, float pMenuItemLocalY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public AllScenes getParentScene() {
+		return AllScenes.MAIN_MENU_SCENE;
+	}
+
+}

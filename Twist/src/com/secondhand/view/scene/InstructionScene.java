151e7a795198308388f16935e43deefbfb82b0e4
Eric Arnebäck <erkastina@hotmail.com>
2013-05-22 11:30:33 +0200

    PMD.

--- Twist/src/com/secondhand/view/scene/InstructionScene.java
+++ Twist/src/com/secondhand/view/scene/InstructionScene.java
@@ -21,7 +21,7 @@ public class InstructionScene extends GameScene {
 
 		final Font mFont = Fonts.getInstance().menuItemFont;
 		//TODO move into Values. both sv and en
-		String instructionText = "You control a black hole by touching the  \n screen. You get points when you eat \n smaller planets and black hole, \n but look up for the bigger black hole. \n There are obstacles in your way and \n powerups you can take if you want.";
+		final String instructionText = "You control a black hole by touching the  \n screen. You get points when you eat \n smaller planets and black hole, \n but look up for the bigger black hole. \n There are obstacles in your way and \n powerups you can take if you want.";
 		
 		final Text instructions = new Text(100, 60, mFont, LocalizationStrings
 				.getInstance().getLocalizedString("menu_instructions"),
@@ -37,7 +37,6 @@ public class InstructionScene extends GameScene {
 
 	@Override
 	public AllScenes getParentScene() {
-		// TODO Auto-generated method stub
 		return AllScenes.MAIN_MENU_SCENE;
 	}
 

7b5ecbde4dacb36c8bdb3da3dc4ddd774cbc0548
Andreas <andreas.wahlstrom@hotmail.com>
2013-05-23 12:27:06 +0200

    testing work

--- TwirlTestTest/src/com/secondhand/model/powerup/RandomPowerUpTest.java
+++ TwirlTestTest/src/com/secondhand/model/powerup/RandomPowerUpTest.java
@@ -9,24 +9,30 @@ import com.secondhand.model.resource.PowerUpType;
 import junit.framework.TestCase;
 
 public class RandomPowerUpTest extends TestCase {
-
-	public void testInstantPowerUp() {
+	class MockRandom extends Random
+	{	
+		int i;
 		
-		// we'll do it for the extra life powerup
+		private static final long serialVersionUID = 1L;
 		
-		class MockRandom extends Random
-		{	
-			private static final long serialVersionUID = 1L;
-
-			public int nextInt(int n)
-			{
-				// always extra life power up
-				return 1;
-			}
+		public MockRandom(int start){
+			i = start;
 		}
+		public int nextInt(int n)
+		{
+			// always extra life power up
+			return i++;
+		}
+	}
+public void testConstructor(){
+		
+	}
+
+	public void testInstantPowerUp() {		
+		// we'll do it for the extra life powerup
 
 		Player player = new Player(new Vector2(), 10f, 3, 0, 100);
-		RandomPowerUp powerup = new RandomPowerUp(new Vector2(), new MockRandom());
+		RandomPowerUp powerup = new RandomPowerUp(new Vector2(), new MockRandom(1));
 		
 		assertEquals(0, powerup.getDuration(), 0.001);
 		assertEquals(6, RandomPowerUp.getFrequency());	
@@ -41,20 +47,9 @@ public class RandomPowerUpTest extends TestCase {
 	
 	public void testTimedPowerUp() {
 		
-		class MockRandom extends Random
-		{	
-			private static final long serialVersionUID = 1L;
-
-			public int nextInt(int n)
-			{
-				// always extra life power up
-				return 0;
-			}
-		}
-
-		
 		Player player = new Player(new Vector2(), 10f, 3, 0, 100);
-		RandomPowerUp powerup = new RandomPowerUp(new Vector2(), new MockRandom());
+
+		RandomPowerUp powerup = new RandomPowerUp(new Vector2(), new MockRandom(0));
 		
 		assertEquals(5, powerup.getDuration(), 0.001);
 		assertEquals(6, RandomPowerUp.getFrequency());	
@@ -74,5 +69,7 @@ public class RandomPowerUpTest extends TestCase {
 		powerup.deactivateEffect(player, false);
 		assertFalse(player.canEatInedibles());
 	}
+	
+	
 	
 }

ae5c0b4e3a953403563c627961977e6921346e1f
LinneaAndersson <Linneamaria10@gmail.com>
2013-05-26 21:55:05 +0200

    fixing randomLevelGeneratorTest

--- TwirlTestTest/src/com/secondhand/model/randomlevelgenerator/sat/RandomLevelGeneratorTest.java
+++ TwirlTestTest/src/com/secondhand/model/randomlevelgenerator/sat/RandomLevelGeneratorTest.java
@@ -6,26 +6,32 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
+import com.secondhand.model.entity.Enemy;
 import com.secondhand.model.entity.Entity;
+import com.secondhand.model.entity.GameSettings;
 import com.secondhand.model.entity.IGameWorld;
+import com.secondhand.model.entity.Obstacle;
+import com.secondhand.model.entity.Planet;
 import com.secondhand.model.entity.Player;
 import com.secondhand.model.entity.PowerUpList;
 import com.secondhand.model.entity.RandomLevelGenerator;
 import com.secondhand.model.physics.IPhysicsWorld;
 import com.secondhand.model.physics.Vector2;
+import com.secondhand.model.powerup.PowerUp;
 import com.secondhand.model.powerup.PowerUpFactory;
 
 public class RandomLevelGeneratorTest extends TestCase {
 
-	private class GameWorldTest implements IGameWorld{
+	private class GameWorldTest implements IGameWorld {
 		int levelNumber;
 		int levelWeight;
 		int levelHeight;
-		
-		public GameWorldTest(int levelNumber){
-			this.levelNumber=levelNumber;
+		public static final float PLAYER_STARTING_SIZE = 30f;
+
+		public GameWorldTest(int levelNumber) {
+			this.levelNumber = levelNumber;
 		}
-		
+
 		@Override
 		public void addListener(PropertyChangeListener listener) {
 			// TODO Auto-generated method stub
@@ -59,13 +65,13 @@ public class RandomLevelGeneratorTest extends TestCase {
 		@Override
 		public int getLevelWidth() {
 			// TODO Auto-generated method stub
-			return levelNumber*1500;
+			return levelNumber * 1500;
 		}
 
 		@Override
 		public int getLevelHeight() {
 			// TODO Auto-generated method stub
-			return levelNumber*1500;
+			return levelNumber * 1500;
 		}
 
 		@Override
@@ -110,13 +116,39 @@ public class RandomLevelGeneratorTest extends TestCase {
 			return 0;
 		}
 	}
-	
-	public void testConstructor(){
+
+	public void testConstructor() {
+		GameSettings.getInstance().setHasEnemies(true);
+		int nObstacles = 0, nPlanets = 0, nEnemies = 0, nPowerUps = 0;
 		GameWorldTest testGameWorld = new GameWorldTest(1);
-		RandomLevelGenerator rLG = new RandomLevelGenerator(testGameWorld,new PowerUpFactory());
-		
+		RandomLevelGenerator rLG = new RandomLevelGenerator(testGameWorld,
+				new PowerUpFactory());
+
+		// Testing public variables
 		assertEquals(testGameWorld.getLevelHeight(), rLG.levelHeight);
 		assertEquals(testGameWorld.getLevelWidth(), rLG.levelWidth);
+		assertEquals(testGameWorld.PLAYER_STARTING_SIZE, rLG.playerInitialSize);
+		assertEquals(testGameWorld.PLAYER_STARTING_SIZE
+				* (testGameWorld.levelNumber + 1), rLG.playerMaxSize * 1f);
 		
+		// Testing the entity and enemyList		
+		for (Entity entity : rLG.entityList) {
+		
+			if (entity instanceof Enemy) {
+				nEnemies++;
+			} else if (entity instanceof Obstacle) {
+				nObstacles++;
+			} else if (entity instanceof Planet) {
+				nPlanets++;
+			} else if (entity instanceof PowerUp) {
+				nPowerUps++;
+			}
+		}
+		assertEquals((7+5+5+14)*testGameWorld.levelNumber,rLG.entityList.size());
+		assertEquals(nEnemies, testGameWorld.levelNumber*5);
+		assertEquals(nObstacles, testGameWorld.levelNumber*5);
+		assertEquals(nPlanets,14*testGameWorld.levelNumber);
+		assertEquals(nPowerUps, testGameWorld.levelNumber*7);
+
 	}
 }

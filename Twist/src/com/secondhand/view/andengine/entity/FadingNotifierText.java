e1e4afd91f96654a864c51fec5c6e493b8a643f7
Andreas <andreas.wahlstrom@hotmail.com>
2013-05-26 19:28:09 +0200

    View.andengine.entity cleaned

--- Twist/src/com/secondhand/view/andengine/entity/FadingNotifierText.java
+++ Twist/src/com/secondhand/view/andengine/entity/FadingNotifierText.java
@@ -15,33 +15,31 @@ import com.secondhand.view.resource.Fonts;
 public class FadingNotifierText extends Text {
 
 	private static final float DURATION = 1.7f;
-	
-	
+
 	public FadingNotifierText(final String str, final Vector2 position) {
 		super(position.x, position.y, Fonts.getInstance().menuItemFont, str);
-		
+
 		// otherwise the alpha channel won't work
 		setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
-		
-		
-		final IEntityModifierListener modifierListener= new IEntityModifierListener() {
+
+		final IEntityModifierListener modifierListener = new IEntityModifierListener() {
 
 			@Override
 			public void onModifierStarted(final IModifier<IEntity> pModifier,
-				final IEntity pItem) {
+					final IEntity pItem) {
 
 			}
 
 			@Override
 			public void onModifierFinished(final IModifier<IEntity> pModifier,
 					final IEntity pItem) {
-				// clean up
 				FadingNotifierText.this.detachSelf();
-			}    	
+			}
 		};
-		
-		registerEntityModifier(new AlphaModifier(DURATION, 1, 0, modifierListener));
+
+		registerEntityModifier(new AlphaModifier(DURATION, 1, 0,
+				modifierListener));
 		registerEntityModifier(new MoveYModifier(DURATION, getY(), getY() - 50));
 	}
-	
+
 }
--- Twist/src/com/secondhand/view/andengine/entity/RocketEmitter.java
+++ Twist/src/com/secondhand/view/andengine/entity/RocketEmitter.java
@@ -18,23 +18,25 @@ import com.secondhand.view.resource.TextureRegions;
 public class RocketEmitter extends ParticleSystem {
 
 	public final static float DURATION = 1;
-	
-	public RocketEmitter(final float surfaceX, final float surfaceY, final float centerX, final float centerY) {
-		super(new CircleOutlineParticleEmitter(surfaceX, surfaceY, 10), 60, 60, 10, 
-				TextureRegions.getInstance().rocketParticleTexture);
-		
+
+	public RocketEmitter(final float surfaceX, final float surfaceY,
+			final float centerX, final float centerY) {
+		super(new CircleOutlineParticleEmitter(surfaceX, surfaceY, 10), 60, 60,
+				10, TextureRegions.getInstance().rocketParticleTexture);
+
 		addParticleInitializer(new ColorInitializer(1, 0, 0));
 		addParticleInitializer(new AlphaInitializer(0));
 		setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE);
-		
+
 		final float factor = 2;
 		final float velocityX = (surfaceX - centerX) * factor;
 		final float velocityY = (surfaceY - centerY) * factor;
-		final VelocityInitializer particleVelocity = new VelocityInitializer(velocityX, velocityY);
+		final VelocityInitializer particleVelocity = new VelocityInitializer(
+				velocityX, velocityY);
 		addParticleInitializer(particleVelocity);
-		               
+
 		addParticleInitializer(new RotationInitializer(0.0f, 360.0f));
-		addParticleModifier(new ScaleModifier(0.5f, 0.1f, 0,1));
+		addParticleModifier(new ScaleModifier(0.5f, 0.1f, 0, 1));
 		addParticleModifier(new ColorModifier(1, 1, 0, 0.5f, 0, 0, 0, 3));
 		addParticleModifier(new ColorModifier(1, 1, 0.5f, 1, 0, 1, 4, 6));
 		addParticleModifier(new AlphaModifier(0, 1, 0, 1));
